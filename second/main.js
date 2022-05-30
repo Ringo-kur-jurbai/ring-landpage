f7c8f48542c525b39975d9613bf94a8ffce1b86f
Ringo-kur-jurbai <106512715+Ringo-kur-jurbai@users.noreply.github.com>
2022-05-30 01:20:50 -0700

    Add files via upload

---
+++ second/main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let center4 = document.querySelector('.center4');
+
+function phones(phone){
+    img.src = phone;;
+}
+function colors(color){
+    center4.style.background = color;
+}
